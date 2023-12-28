3dbf2e496bcc764445df8928c2148c3a54499e65
Ale Valdivia <alexandervaldivia.thoughtworks@latam.com>
2023-12-28 09:04:02 -0300

    removing terragrunt

--- main.tf
+++ main.tf
@@ -1,6 +1,32 @@
 provider "confluent" {
-  # kafka_id            = var.kafka_id
-  # kafka_rest_endpoint = var.kafka_rest_endpoint
-  # kafka_api_key       = var.kafka_api_key
-  # kafka_api_secret    = var.kafka_api_secret
+}
+
+locals {
+  kafka_sa_environment = var.environment == "cert" ? "intg" : var.environment
+
+  this = yamldecode(var.this_yaml)
+
+  sa = [
+    for name in local.this.service_accounts : {
+      service_account_name = "${var.it_element}-${var.environment}-${name.service_account_name}"
+    }
+  ]
+
+  topics = [
+    for topic in local.this.topics : {
+      topic_name = "${local.this.it_element}-${var.environment}-${topic.topic_name}"
+      acls       = [
+        for acl in topic.acls : {
+          service_account_name = "${var.it_element}-${var.environment}-${acl.service_account_name}"
+          # Add other ACL properties here
+        }
+      ]
+      cgs        = [
+        for cg in topic.cgs : {
+          service_account_name = "${var.it_element}-${var.environment}-${cg.service_account_name}"
+          # Add other CG properties here
+        }
+      ]
+    }
+  ]
 }
